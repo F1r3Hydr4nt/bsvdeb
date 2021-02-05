7513db452ebdb9ebdbe6c8ea965bff66727c9bcc
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2021-02-05 18:30:24 +0900

    f

--- ci/test/00_setup_env_s390x_dgrs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Copyright (c) 2019-2020 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
-. $(dirname $0)/00_setup_env_s390x.sh
-
-export BITCOIN_CONFIG="--enable-dangerous $BITCOIN_CONFIG"
